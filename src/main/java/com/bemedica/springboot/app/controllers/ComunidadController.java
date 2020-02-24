9c8b81673a4b533dd81c0047425ff7efa397d649
CoDorsa <59836826+CoDorsa@users.noreply.github.com>
2020-02-24 09:38:03 -0600

    Update ComunidadController.java

--- src/main/java/com/bemedica/springboot/app/controllers/ComunidadController.java
+++ src/main/java/com/bemedica/springboot/app/controllers/ComunidadController.java
@@ -1,12 +1,15 @@
 package com.bemedica.springboot.app.controllers;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
+import com.bemedica.springboot.app.models.entity.User;
 import com.bemedica.springboot.app.service.ComunidadService;
+import com.bemedica.springboot.app.service.UserService;
 
 @Controller
 public class ComunidadController {
@@ -14,12 +17,14 @@ public class ComunidadController {
 	
 	@Autowired
 	private ComunidadService com;
+	@Autowired
+	UserService userService;
 	
 	@RequestMapping(value="/comunidad-bemedica", method=RequestMethod.GET)
-	public String listarMedicos(Model model) {
-		
-		model.addAttribute("medico",com.AppCitasSitie());
-		return "comunidad-bemedica";
+	public String listarMedicos(Model model ,HttpServletRequest request) throws Exception {
+	model.addAttribute("medico",com.AppCitasSitie());
+	model.addAttribute("user", userService.getSession(request));
+	return "comunidad-bemedica";
 	}
 
 }
