4e42eaa5d1bc49aa860c33c91d40c83752d5d890
LeonardoTiDev <60356628+LeonardoTiDev@users.noreply.github.com>
2020-03-06 10:28:22 -0600

    aqui van los cambios que Don Vic no jalo chido

--- src/main/java/com/bemedica/springboot/app/controllers/ResultadosController.java
+++ src/main/java/com/bemedica/springboot/app/controllers/ResultadosController.java
@@ -6,7 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.bemedica.springboot.app.Dao.IResultadoEmpresaDao;
 import com.bemedica.springboot.app.service.UserService;
 
 @Controller
@@ -14,6 +18,10 @@ public class ResultadosController {
 	
 	@Autowired
 	UserService userService;
+	
+	
+	@Autowired
+	IResultadoEmpresaDao resultadoEmpresa;
 	
 	
 	@GetMapping(value="/resultados/paciente")
@@ -32,5 +40,32 @@ public class ResultadosController {
 	public String Resultempleados(Model model ,HttpServletRequest request) throws Exception{
 	model.addAttribute("user", userService.getSession(request));
 	return"resultados/empleados";
+	}
+	
+	
+	
+
+	@RequestMapping(value = "/resultados/empresa/{id}", method = RequestMethod.GET)
+	public String listarResul(@PathVariable(value = "id") int id, Model model, HttpServletRequest request)
+			throws Exception {
+
+		model.addAttribute("user", userService.getSession(request));
+		model.addAttribute("vista", resultadoEmpresa.findResul(id));
+
+		System.out.println(id);
+		return "resultados/empresa";
+	} 
+	
+	
+
+	@RequestMapping(value = "/orden-empresa/{id}/{id2}")
+	public String listarResulporOrden(@PathVariable(value = "id") int id, @PathVariable(value = "id2") int id2,
+			Model model, HttpServletRequest request) throws Exception {
+
+		model.addAttribute("user", userService.getSession(request));
+		model.addAttribute("vistaordenes", resultadoEmpresa.findOrden(id, id2));
+		System.out.println(id +" "+ id2);
+
+		return "resultados/orden-empresa";
 	}
 }
